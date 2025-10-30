e38c6240ae7bae303b1de850147c174a5a7e2db6
nickyichov <nikiichov99@gmail.com>
2025-10-30 12:49:30 +0100

    Add showcase image

--- src/Sensors.jsx
+++ src/Sensors.jsx
@@ -1,8 +1,10 @@
 export default function Sensors({ temperature }) {
    return (
        <>
-           <Temperature temperature={temperature} />
-           <Camera/>
+           <div className="flex">
+               <Camera/>
+               <Temperature temperature={temperature} />
+           </div>
        </>
    )
 }
@@ -11,7 +13,7 @@ function Camera() {
     return (
         <>
             <div className="w-56 h-56 border flex items-center justify-center m-2">
-
+                <img className="h-full" src="../src/assets/thermalPhoto.jpeg" alt="thermal photo" />
             </div>
         </>
     )
@@ -21,9 +23,9 @@ function Temperature({ temperature }) {
     return (
         <>
             <div className={
-                temperature > 65 && temperature < 75 ? "w-24 border p-2 m-2 flex items-center justify-center bg-orange-500" :
-                temperature > 75 ? "w-24 border p-2 m-2 flex items-center justify-center bg-red-500"
-                : "w-24 border p-2 m-2 flex items-center justify-center bg-green-500"}>
+                temperature > 60 && temperature < 70 ? "w-24 h-12 border p-2 m-2 flex items-center justify-center bg-orange-500" :
+                temperature > 70 ? "w-24 h-12 border p-2 m-2 flex items-center justify-center bg-red-500"
+                : "w-24 h-12 border p-2 m-2 flex items-center justify-center bg-green-500"}>
                 {temperature.toFixed(1)}&#8451;
             </div>
         </>
