3da79eccd48ba226adec207aa7fe1c209e3f95f1
nickyichov <nikiichov99@gmail.com>
2025-11-19 13:52:28 +0100

    remove unused code (temperature function)

--- src/Sensors.jsx
+++ src/Sensors.jsx
@@ -47,55 +47,13 @@ export default function Sensors({ roll, pitch, yaw, temperature, airspeed, groun
                            </div>
                        }
                    </div>
-                   <div className="flex h-1/2 items-end p-2">
+                   <div className="flex h-1/2 items-start p-2">
                        <ConstantSensors temperature={temperature} />
                    </div>
                </div>
            </div>
        </>
    )
-}
-
-function Temperature({ temperature }) {
-    return (
-        <>
-            <div className="flex flex-col items-end">
-                <div className="flex flex-col items-center">
-                    <div className={
-                        temperature > 60 && temperature < 70 ? "w-24 h-12 border p-2 m-2 mb-0 flex items-center justify-center bg-orange-500" :
-                        temperature > 70 ? "w-24 h-12 border p-2 m-2 mb-0 flex items-center justify-center bg-red-500"
-                        : "w-24 h-12 border p-2 m-2 mb-0 flex items-center justify-center bg-green-500"}>
-                        {temperature.toFixed(1)}&#8451;
-                    </div>
-                    <div className="bg-white w-24 text-black flex justify-center text-xs text-center">
-                        Pixhawk Temperature
-                    </div>
-                </div>
-                <div className="flex flex-col items-center">
-                    <div className={
-                        temperature > 60 && temperature < 70 ? "w-24 h-12 border p-2 m-2 mb-0 flex items-center justify-center bg-orange-500" :
-                            temperature > 70 ? "w-24 h-12 border p-2 m-2 mb-0 flex items-center justify-center bg-red-500"
-                                : "w-24 h-12 border p-2 m-2 mb-0 flex items-center justify-center bg-green-500"}>
-                        {temperature.toFixed(1)}&#8451;
-                    </div>
-                    <div className="bg-white w-24 text-black flex justify-center text-xs text-center">
-                        Fire Temperature
-                    </div>
-                </div>
-                <div className="flex flex-col items-center">
-                    <div className={
-                        temperature > 60 && temperature < 70 ? "w-24 h-12 border p-2 m-2 mb-0 flex items-center justify-center bg-orange-500" :
-                            temperature > 70 ? "w-24 h-12 border p-2 m-2 mb-0 flex items-center justify-center bg-red-500"
-                                : "w-24 h-12 border p-2 m-2 mb-0 flex items-center justify-center bg-green-500"}>
-                        {temperature.toFixed(1)}&#8451;
-                    </div>
-                    <div className="bg-white w-24 text-black flex justify-center text-xs text-center">
-                        Air Temperature
-                    </div>
-                </div>
-            </div>
-        </>
-    )
 }
 
 function ConstantSensors({ temperature }) {
